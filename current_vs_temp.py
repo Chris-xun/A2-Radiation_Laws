6ac550b8cec9e3c0d758b46980f0bac9f54a3d05
Chris-xun <yx20021225@163.com>
2024-03-19 15:38:51 +0000

    d

--- current_vs_temp.py
+++ current_vs_temp.py
@@ -24,18 +24,39 @@ def load_file_custom(path):
 
     return np_data
 
+# blackbody radiation function
+def blackbody_radiation(lambdas, T , A):
+    c = 2.99792e8
+    h = 6.626e-34
+    k_b = 1.380649e-23
+    value =  A * (2*h*c**2) / ((lambdas)**5 * (np.exp(h*c / ((lambdas)*k_b*T)) - 1))
+    return value
+
 
 
 # all the files names
 current_numbers = [0.4, 0.45, 0.5, 0.55, 0.6, 0.65, 0.7, 0.75, 0.8, 0.85]
+colors = ['red', 'blue', 'green', 'orange', 'purple', 'black', 'pink', 'yellow', 'brown', 'grey']
+
+# current_numbers = [0.4]
+# colors = ['red']
 
 # plotting all the files onto 1 graph for comparison
-for current in current_numbers:
+for current, color in zip(current_numbers, colors):
     # importing the data
     data = load_file_custom(f'data\section_of_coil__current_vs_temp\half_way_{current}A.csv')
-    x = data[:-2, 0]
-    y = data[:-2, 1]
-    plt.plot(x, y, label=str(current)+'A')
+    
+    # normalising the data
+    x = data[:, 0] * 1e-9
+    y = data[:, 1] / np.trapz(data[:,1], x)
+
+    data_slicing_posiion = len(x) // 5 * 3
+    optimal_params, cov_matrix = opt.curve_fit(blackbody_radiation, x[:data_slicing_posiion], y[:data_slicing_posiion], p0=[1000, 0.5])
+    print(optimal_params[0])
+    # print(blackbody_radiation(x, optimal_params[0]))
+    fit_y = blackbody_radiation(x, *optimal_params) #/ np.max(blackbody_radiation(x, *optimal_params))
+    plt.plot(x, y, label=str(current)+'A', color=color)
+    plt.plot(x, fit_y, color=color, linestyle='--')
 
 plt.xlabel('Wavelength')
 plt.ylabel('Intensity')
