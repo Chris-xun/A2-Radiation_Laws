e57db45aab7a9ef422c968b19e91913be84765bf
Gokules-the-first <gokulam29@gmail.com>
2024-03-31 20:58:44 +0100

    new heatmap tech

--- current_vs_temp_for_heatmap.py
+++ current_vs_temp_for_heatmap.py
@@ -9,6 +9,9 @@ import scipy.optimize as opt
 import functions as f
 import os
 import coil_heatmap as h
+import scienceplots
+plt.style.use(['science', 'notebook'])
+plt.rcParams['font.family'] = 'Times New Roman'
 
 # defining the functions
 # skipping tbe irrelevant lines
@@ -52,7 +55,9 @@ for file in files:
     # data_slicing_posiion = len(x) // 5 * 3
     data_slicing_posiion = (np.where(y>0.1e7)[0][-1] - np.where(y>0.1e7)[0][0]) // 6 + np.where(y>0.1e7)[0][0]
     optimal_params, cov_matrix = opt.curve_fit(blackbody_radiation, x[:data_slicing_posiion], y[:data_slicing_posiion], p0=[1000, 0.5])
-    print(optimal_params[0])
+    print(optimal_params[0], np.sqrt(cov_matrix[0, 0]))
+    
+    
     # print(blackbody_radiation(x, optimal_params[0]))
     fit_y = blackbody_radiation(x, *optimal_params) #/ np.max(blackbody_radiation(x, *optimal_params))
     plt.plot(x, y, label=data, alpha = 0.5)
@@ -75,4 +80,30 @@ for file in files:
 print(temp)
 
 # plotting the data in a heat map
-h.interpolate_heatmap(temp, 1, 1)
+# h.interpolate_heatmap(temp, 1, 1)
+
+# trying to showcase the information more clearly 
+plt.figure(figsize=(5,2))
+plt.title('Tungsten Coil Heat Distrabution', fontsize = 25)
+im = plt.imshow(temp, cmap='plasma', interpolation='gaussian', origin = 'lower')
+for i in range(temp.shape[0]):
+    for j in range(temp.shape[1]):
+        plt.text(j, i, f'{int(round(temp[i, j]))}', ha='center', 
+                 va='bottom', color='white', fontsize=20, fontweight='bold', )
+y_ticks_mm = np.linspace(0, 2.5, 6)  # Generate ticks at 0, 0.5, 1, 1.5, 2, and 2.5 mm
+y_ticks_units = np.linspace(0, 5, 6)  # Corresponding tick positions in original units
+
+x_ticks_mm = np.linspace(0, 2, 3)  # Generate ticks at 0, 0.5, 1, 1.5, 2, and 2.5 mm
+x_ticks_units = np.linspace(0, 2, 3)
+
+plt.yticks(y_ticks_units, y_ticks_mm, fontsize = 20)  # Set the ticks with the rescaled values
+plt.xticks(x_ticks_units, x_ticks_mm, fontsize = 20) 
+cbar = plt.colorbar(im)
+cbar.set_label('Temperature [K]', rotation=270, labelpad=25, fontsize = 25)
+cbar.ax.tick_params(labelsize = 20)
+
+plt.xlabel('X position [cm]', fontsize = 25)
+plt.ylabel('Y position [cm]', fontsize = 25)
+plt.show()
+
+print(temp[:, 1])
